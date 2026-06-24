e88b124495354f1476d4be35f34b03995e599480
illay12360-cmyk <illay12360@gmail.com>
2026-06-24 18:45:52 +0300

    Створення структури

---
+++ Lab2/LR_2_task_1.py
@@ -0,0 +1,68 @@
+import numpy as np
+from sklearn import preprocessing
+from sklearn.svm import LinearSVC
+from sklearn.multiclass import OneVsOneClassifier
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score
+
+input_file = 'income_data.txt'
+
+X = []
+y = []
+count_class1 = 0
+count_class2 = 0
+max_datapoints = 25000
+
+with open(input_file, 'r') as f:
+    for line in f.readlines():
+        if count_class1 >= max_datapoints and count_class2 >= max_datapoints:
+            break
+        if '?' in line:
+            continue
+        data = line[:-1].split(', ')
+        if data[-1] == '<=50K' and count_class1 < max_datapoints:
+            X.append(data)
+            count_class1 += 1
+        elif data[-1] == '>50K' and count_class2 < max_datapoints:
+            X.append(data)
+            count_class2 += 1
+
+X = np.array(X)
+
+label_encoder = []
+X_encoded = np.empty(X.shape, dtype=object)
+
+for i, item in enumerate(X[0]):
+    if item.isdigit():
+        X_encoded[:, i] = X[:, i]
+    else:
+        le = preprocessing.LabelEncoder()
+        X_encoded[:, i] = le.fit_transform(X[:, i])
+        label_encoder.append(le)
+
+X = X_encoded[:, :-1].astype(int)
+y = X_encoded[:, -1].astype(int)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=5)
+
+classifier = OneVsOneClassifier(LinearSVC(random_state=0, dual="auto"))
+classifier.fit(X_train, y_train)
+
+y_test_pred = classifier.predict(X_test)
+print(f"F1 score: {round(100 * f1_score(y_test, y_test_pred, average='weighted'), 2)}%")
+print(f"Accuracy: {round(100 * accuracy_score(y_test, y_test_pred), 2)}%")
+print(f"Precision: {round(100 * precision_score(y_test, y_test_pred, average='weighted'), 2)}%")
+print(f"Recall: {round(100 * recall_score(y_test, y_test_pred, average='weighted'), 2)}%")
+
+input_data = ['37', 'Private', '215646', 'HS-grad', '9', 'Never-married', 'Handlers-cleaners', 'Not-in-family', 'White', 'Male', '0', '0', '40', 'United-States']
+input_data_encoded = [-1] * len(input_data)
+count = 0
+for i, item in enumerate(input_data):
+    if item.isdigit():
+        input_data_encoded[i] = int(input_data[i])
+    else:
+        input_data_encoded[i] = int(label_encoder[count].transform([input_data[i]])[0])
+        count += 1
+
+predicted_class = classifier.predict([input_data_encoded])
+print("Результат класифікації:", label_encoder[-1].inverse_transform(predicted_class)[0])
---
+++ Lab2/LR_2_task_2_1.py
@@ -0,0 +1,49 @@
+import numpy as np
+from sklearn import preprocessing
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score
+
+input_file = 'income_data.txt'
+X, y = [], []
+count_class1, count_class2 = 0, 0
+max_datapoints = 5000 # Зменшено для швидкості навчання нелінійних ядер
+
+with open(input_file, 'r') as f:
+    for line in f.readlines():
+        if count_class1 >= max_datapoints and count_class2 >= max_datapoints:
+            break
+        if '?' in line:
+            continue
+        data = line[:-1].split(', ')
+        if data[-1] == '<=50K' and count_class1 < max_datapoints:
+            X.append(data)
+            count_class1 += 1
+        elif data[-1] == '>50K' and count_class2 < max_datapoints:
+            X.append(data)
+            count_class2 += 1
+
+X = np.array(X)
+X_encoded = np.empty(X.shape, dtype=object)
+for i, item in enumerate(X[0]):
+    if item.isdigit():
+        X_encoded[:, i] = X[:, i]
+    else:
+        le = preprocessing.LabelEncoder()
+        X_encoded[:, i] = le.fit_transform(X[:, i])
+
+X = X_encoded[:, :-1].astype(int)
+y = X_encoded[:, -1].astype(int)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=5)
+
+classifier = SVC(kernel='poly', degree=3)
+classifier.fit(X_train, y_train)
+
+# 4. Оцінка результатів
+y_test_pred = classifier.predict(X_test)
+print("--- Гаусове ядро (RBF) ---")
+print(f"F1 score: {round(100 * f1_score(y_test, y_test_pred, average='weighted'), 2)}%")
+print(f"Accuracy: {round(100 * accuracy_score(y_test, y_test_pred), 2)}%")
+print(f"Precision: {round(100 * precision_score(y_test, y_test_pred, average='weighted'), 2)}%")
+print(f"Recall: {round(100 * recall_score(y_test, y_test_pred, average='weighted'), 2)}%")
